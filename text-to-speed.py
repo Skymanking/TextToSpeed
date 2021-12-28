89f6075b8947c1182a02870af01e415e25ad9257
Sky Man King <skymankingtam@gmail.com>
2021-12-28 11:29:35 +0700

    add

--- text-to-speed.py
+++ text-to-speed.py
@@ -1,10 +1,9 @@
 import gtts
 from playsound import playsound
-with open('text.txt', 'r', encoding='UTF-8') as f:
+with open('Mau.txt', 'r', encoding='UTF-8') as f:
     data = f.read()
 print(data)
 tts = gtts.gTTS(data, lang="vi")
 tts.save("hello.mp3")
-playsound("hello.mp3")
 f.close()
 
